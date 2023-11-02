cc77184f741ba9f6ce0d594a8e7080a67e2fad3f
renukanandatd <renukananda2000@gmail.com>
2023-11-02 11:28:58 +0530

    FINAL COMMIT

--- app/src/main/java/com/example/rockpaperscissor/RockPaperScissor.kt
+++ app/src/main/java/com/example/rockpaperscissor/RockPaperScissor.kt
@@ -8,4 +8,30 @@ fun main(){
     println("Rock, Paper or Scissors? Please enter your choice")
     yourChoice = readln()
 
+    val randomNumber = (1..3).random()
+
+    when (randomNumber) {
+        1 -> {
+            computerChoice = "ROCK"
+        }
+        2 -> {
+            computerChoice = "PAPER"
+        }
+        else -> {
+            computerChoice = "SCISSOR"
+        }
+    }
+
+    println(computerChoice)
+
+    val winner = when{
+        computerChoice == yourChoice -> "TIE"
+        computerChoice == "ROCK" && yourChoice == "PAPER" -> "PLAYER"
+        computerChoice == "PAPER" && yourChoice == "SCISSOR" -> "PLAYER"
+        computerChoice == "SCISSOR" && yourChoice == "ROCK" -> "PLAYER"
+        else -> "COMPUTER"
+    }
+
+    println("The winner is $winner!!!")
+
 }
