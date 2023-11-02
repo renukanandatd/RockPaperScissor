5f1d7c0209f625caf94e16e934fe84b688d55f62
renukanandatd <renukananda2000@gmail.com>
2023-11-02 10:46:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rockpaperscissor/RockPaperScissor.kt
@@ -0,0 +1,11 @@
+package com.example.rockpaperscissor
+
+fun main(){
+
+    var computerChoice = ""
+    var yourChoice = ""
+
+    println("Rock, Paper or Scissors? Please enter your choice")
+    yourChoice = readln()
+
+}
